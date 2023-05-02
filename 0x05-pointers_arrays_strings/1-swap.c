e00000d2a18984e0511026ec38177a724b9905bc
SalwaMahmoud9 <sma.mam9@gmail.com>
2023-05-02 05:13:29 -0700

    pointers-task from 1 to 10

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps integers
+ *
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	int d = *b;
+	*a = d;
+	*b = c;
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _strcpy - copies the string pointed to by src
+ *
+ * @dest: destination
+ * @src: src
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
