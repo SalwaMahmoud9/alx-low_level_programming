ece48d864ca0170d5c9d582da0dd95c6cb6c569d
Salwa Mahmoud <108579776+SalwaMahmoud9@users.noreply.github.com>
2023-10-30 23:38:50 +0200

    Update 0-main.c

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -7,10 +7,15 @@
  *
  * Return: Always 0.
  */
-int main(char **av)
+int main(int ac, char **av)
 {
     ssize_t n;
 
+    if (ac != 2)
+    {
+        dprintf(2, "Usage: %s filename\n", av[0]);
+        exit(1);
+    }
     n = read_textfile(av[1], 114);
     printf("\n(printed chars: %li)\n", n);
     n = read_textfile(av[1], 1024);
