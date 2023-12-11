466f4f03d6114f367608bcc1fead34fbad12ee7f
SalwaMahmoud9 <sma.mam9@gmail.com>
2023-12-11 09:07:58 +0200

    11-12-2023

---
+++ 0x18-dynamic_libraries/tests/0-main.c
@@ -0,0 +1,13 @@
+#include "../holberton.h"
+#include <stdio.h>
+
+/**
+ * main - main
+ *
+ * Return: EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("Holberton"));
+	return (EXIT_SUCCESS);
+}
